ed36c71c65dfe38fc5e9cef614bf4dca3a890e74
Zain-The-Coder <zain015976@gmail.com>
2025-09-19 22:20:12 +0500

    FireBase Project 1

---
+++ Project no 1 - Testing/src/index.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app' ;
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyBtRFi_2uOfq1w7LarPimRZ-WHN12fP1fQ",
+  authDomain: "fir-project-1-b9b98.firebaseapp.com",
+  projectId: "fir-project-1-b9b98",
+  storageBucket: "fir-project-1-b9b98.firebasestorage.app",
+  messagingSenderId: "377121079883",
+  appId: "1:377121079883:web:511e574e9667bfc79f83ce",
+  measurementId: "G-ZEGXCVT2RS"
+}) ;
