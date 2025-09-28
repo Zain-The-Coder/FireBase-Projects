045bb525bdd7f0a801010128a86a776bf6415473
Zain-The-Coder <zain015976@gmail.com>
2025-09-28 14:57:58 +0500

    FireBase Project

--- Project 3 . Crud Application/app.js
+++ Project 3 . Crud Application/app.js
@@ -1,49 +1,56 @@
-let signUp = document.getElementById('sign-up');
-let signIn = document.getElementById("sign-in");
+const { useId } = require("react");
+
 let userEmail = document.getElementById("user-email");
 let userPassword = document.getElementById("user-password");
 let message = document.getElementById("message");
-let signOut = document.getElementById("sign-out");
 let noti = document.getElementById("messanger");
+let taskTaker = document.getElementById("task-taker");
+let div = document.getElementById("container");
+let userName = document.getElementById("user-name");
+let spiner = document.getElementById("loader");
+let user = document.getElementById("username");
+const db = firebase.firestore();
 
-
-signUp.addEventListener("click"  , () => {
+function signUp () {
     firebase.auth().createUserWithEmailAndPassword(userEmail.value, userPassword.value)
   .then((userCredential) => { 
     var user = userCredential.user;
     message.innerHTML = "Login Successfully !" ;
     message.style.color = "green" ;
-    console.log(user);
+    goToHome()
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-    message.innerHTML = "Error Found !" ;
+    let errorMsg = String(errorMessage).slice(9) ;
+    message.innerHTML = errorMsg ;
     message.style.color = "red" ;
     console.log(errorCode) ;
     console.log(errorMessage)
   });
-});
+};
 
 
-signIn.addEventListener("click" , () => {
+function signIn () {
     firebase.auth().signInWithEmailAndPassword(userEmail.value, userPassword.value)
   .then((userCredential) => {
     var user = userCredential.user;
+    localStorage.setItem("userName" , userName.value)  ;  
     message.innerHTML = "Sign In Successfully !" ;
     message.style.color = "green" ;
     console.log(user);
     goToHome() ;
-  })
+})
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
     console.log(errorCode) ;
     console.log(errorMessage);
-    message.innerHTML = "Error Is Found !" ;
+    let errorMsg = String(errorMessage).slice(9) ;
+    message.innerHTML = errorMsg ;
     message.style.color = "red" ;
   });
-});
+};
 
 
 function goToHome () {
@@ -51,15 +58,48 @@ function goToHome () {
 }
 
 function goToIndex () {
-    window.location.href = "./index.html" ;
+  window.location.href = "./index.html" ;
 }
 
-signOut.addEventListener("click" , () => {
-    firebase.auth().signOut().then(() => {
-        noti.innerHTML = "Sign Out  SuccessFully !" ;
-        goToIndex() ;
+function signOut () {
+  goToIndex();
+  localStorage.removeItem("userName");
 
-}).catch((error) => {
-    noti.innerHTML = "An error Happened !" ;
+
+}
+function saverTask () {
+db.collection("users").add({
+    born: 1815  
+})
+.then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
 });
-});
+}
+// let unorderedList = div.firstElementChild ;
+// let liEl = document.createElement("li");
+// unorderedList.appendChild(liEl);
+// liEl.textContent = taskTaker.value;
+
+function saverTask () {
+  spiner.style.visibility = "visible" ;
+  db.collection("test").get()
+  .then(snapshot => {
+    console.log("Firestore Connected ✅");
+    spiner.style.visibility = "hidden" ;
+
+  })
+  .catch(err => {
+    console.error("Firestore Error ❌", err);
+  })
+  .finally(() => {
+    spiner.style.visibility =  "hidden"
+  })
+  
+}
+
+function refresh () {
+  user.innerHTML = localStorage.getItem("userName");
+}
