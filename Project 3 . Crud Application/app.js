06e0c6f0539f9292c15b285e125d29632838595b
Zain-The-Coder <zain015976@gmail.com>
2025-10-06 01:27:51 +0500

    FireBase Project

--- Project 3 . Crud Application/app.js
+++ Project 3 . Crud Application/app.js
@@ -11,14 +11,12 @@ const db = firebase.firestore();
 
 function signUp () {
   localStorage.setItem("userName" , userName.value);
-  localStorage.setItem("userId" , currentUser.uid)
   spiner.style.visibility = "visible" ;
   firebase.auth().createUserWithEmailAndPassword(userEmail.value, userPassword.value)
   .then((userCredential) => {
     let user = userCredential.user;
     spiner.style.visibility = "hidden" ;
     console.log("user added");
-    return firebase.auth() ;
   })
   .catch((error) => {
     //let errorCode = error.code;
@@ -38,8 +36,7 @@ function signIn () {
     console.log("yes, user found");
     spiner.style.visibility = "hidden" ;
     gotoHome() ;
-    localStorage.setItem("userId" , currentUser.uid);
-    return firebase.auth() ;
+    localStorage.setItem("userId" , user.uid);
   })
   .catch((error) => {
     //let errorCode = error.code;
@@ -58,7 +55,6 @@ firebase.auth().signOut()
     localStorage.removeItem("userName");
     localStorage.removeItem("userId");
     spiner.style.visibility = "hidden" ;  
-    return firebase.auth() ;
 }).catch((error) => {
   console.log(error);
 });
@@ -85,9 +81,10 @@ firebase.auth().onAuthStateChanged((user) => {
 });
 
 function saverTask() {
+
 db.collection("tasks").add({
     userName : localStorage.getItem("userName"),
-    userUid : currentUser.uid ,
+    userUid : localStorage.getItem("userId"),
     userTask : task.value ,
 })
 .then((docRef) => {
@@ -103,24 +100,92 @@ function refresh () {
     .onSnapshot((snapshot) => {
       snapshot.docChanges().forEach((change) => {
         if (change.type === "added") {
-          // console.log("New city: ", change.doc.data(), change.doc.id);
           addTask(change.doc);
         }
         // if (change.type === "modified") {
         //     // console.log("Modified city: ", change.doc.data());
         //     updateTodoFromDom(change.doc);
         // }
-        // if (change.type === "removed") {
-        //   // console.log("Removed city: ", change.doc.data());
-        //   deleteFromDom(change.doc);
-        // }
+        if (change.type === "removed") {
+          deleter(change.doc);
+        }
       });
     });
 }
 
 function addTask (parameter) {
+  let heading = document.createElement("h2");
+  heading.textContent = "Remaining Task" ;
   let addUserTask = parameter.data().userTask;
   let liEl = document.createElement("li");
   liEl.textContent = addUserTask;
-  ulEl.appendChild(liEl);
+
+  let container = document.createElement("div");
+  container.style.margin = "auto" ;
+  ulEl.appendChild(container);
+
+  let diver = document.createElement("div");
+  container.appendChild(heading);
+  container.appendChild(diver);
+  diver.appendChild(liEl);
+    diver.style.border = "3px solid black";
+    diver.style.width = "70%"
+    diver.style.margin = "12px 0px";
+    diver.style.padding = "10px";
+    diver.style.listStyleType = "none";
+    diver.style.borderRadius = "10px";
+    diver.style.height = "60px";
+
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete Task" ;
+  deleteButton.style.backgroundColor = "red" ;
+  deleteButton.style.color = "white" ;
+  deleteButton.style.fontWeight = "bold" ;
+  deleteButton.style.width = "120px";
+  deleteButton.style.borderRadius = "2px" ;
+  deleteButton.style.fontFamily = "poppins" ;
+  deleteButton.setAttribute("onClick" , "deleter(this)")
+
+  let editButton = document.createElement("button");
+  editButton.textContent = "Edit Task" ;
+  editButton.style.backgroundColor = "blue" ;
+  editButton.style.color = "white" ;
+  editButton.style.fontWeight = "bold" ;
+  editButton.style.width = "120px";
+  editButton.style.borderRadius = "2px" ;
+  editButton.style.fontFamily = "poppins"
+
+  let completedButton = document.createElement("button");
+  completedButton.textContent = "Completed" ;
+  completedButton.style.backgroundColor = "green" ;
+  completedButton.style.color = "white" ;
+  completedButton.style.fontWeight = "bold" ;
+  completedButton.style.width = "120px";
+  completedButton.style.borderRadius = "2px" ;
+  completedButton.style.fontFamily = "poppins";
+
+  let buttonDiv = document.createElement("div");
+  diver.appendChild()
+
+  diver.appendChild(deleteButton)
+  diver.appendChild(editButton)
+  diver.appendChild(completedButton);
+}
+
+
+function deleter () {
+
+  db.collection("tasks").doc("ulEl")
+  .delete()
+  .then(() => {
+    console.log("Document successfully deleted!");
+}).catch((error) => {
+    console.error("Error removing document: ", error);
+});
+  // let removalDiv = deleter.parentNode.parentNode ;
+  // let parent = removalDiv.parentNode;
+  // parent.removeChild(removalDiv);
+  // deleter.id = user.
+  // console.log(deleter)
+
 }
